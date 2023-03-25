a15a4642beb80833c5e7000ddc5004ca2857491e
Yash-Ganji <127886106+Yash-Ganji@users.noreply.github.com>
2023-03-25 23:37:25 +0530

    Update mux4x1.v

--- day_1/mux4x1/mux4x1.v
+++ day_1/mux4x1/mux4x1.v
@@ -1,1 +1,21 @@
-
+`timescale 1ns / 1ps
+module mux4x1(A, B, C, D, sel, out);
+    input A, B, C, D;
+    input [1:0] sel;
+    output wire out;
+    
+    assign out = sel[1] ? sel[0]? D:C : sel[0]? B:A;
+    
+    // using case statement // declare out as "reg" for case statement implementation
+    /*
+    always@(*)
+    begin
+    case (sel)
+        2'b00 : out <= A;
+        2'b01 : out <= B;
+        2'b10 : out <= C;
+        default : out <= D;
+    endcase
+    end
+    */
+endmodule
