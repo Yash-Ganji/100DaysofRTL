2ab958077535f976fdd7508d50d31137a4c0485c
Yash-Ganji <127886106+Yash-Ganji@users.noreply.github.com>
2023-03-29 00:55:56 +0530

    Update bin_2_grey.v

--- day_4/binary_to_grey_converter/bin_2_grey.v
+++ day_4/binary_to_grey_converter/bin_2_grey.v
@@ -1,1 +1,15 @@
-
+`timescale 1ns / 1ps
+module bin_2_grey
+        #(parameter n=3)      //n is number of bits in binary number.
+        (bin, grey);
+    input [n-1:0] bin;
+    output [n-1:0] grey;
+    
+    assign grey[n-1] = bin[n-1];
+    
+    genvar i;
+    for(i=0; i<n-1; i=i+1)
+    begin
+        assign grey[i] = bin[i+1]^bin[i];
+    end
+endmodule
